aa769bc09213bfe604a340b43582628935c00a06
Gustavo Ramos <gustavoramosrock@gmail.com>
2021-12-12 22:04:23 -0300

    configurando fonte externas

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d37afff2a1466ce0"; }
+/******/ 		__webpack_require__.h = function() { return "7c143b9d2c77c828"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.557ac485810fc25a.hot-update.js
+++ .next/static/webpack/webpack.557ac485810fc25a.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "8b70b2438eafcbf2"; }
+/******/ 	__webpack_require__.h = function() { return "7c143b9d2c77c828"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
