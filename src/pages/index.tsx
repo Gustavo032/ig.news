e079b924e15d8dd1e1069d23fc64bc7068e28e28
Gustavo Ramos <gustavoramosrock@gmail.com>
2021-12-18 19:01:56 -0300

    arrumando importaçao do head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,15 @@
-import { Head } from 'next/document'
-import styles  from '../styles/home.module.scss'
+import Head from 'next/head'
+import Styles  from '../styles/home.module.scss'
 
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>ig.news</title>
+        <title>Início | Ignews</title>
       </Head>
-
-      <h1 className={styles.title}>
-        Hello 
-
-        <span> World! </span> 
+      <h1 className={Styles.title}>
+        Hello <span>World!</span> 
       </h1>
     </>
   );
