a47733c426f10d6eb6d25aa86478b5acacfa9b83
Gustavo Ramos <gustavoramosrock@gmail.com>
2021-12-19 01:58:56 -0300

    Componente Header

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8dd17a2d8c5b10ff"; }
+/******/ 		__webpack_require__.h = function() { return "6384c8bf7142151e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
