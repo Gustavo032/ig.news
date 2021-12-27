eb3fb90394ff15fdb0ea799bfef5f6fe78a8c7ca
Gustavo Ramos <gustavoramosrock@gmail.com>
2021-12-26 22:53:57 -0300

    home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,25 @@
 import Head from 'next/head'
-import Styles  from '../styles/home.module.scss'
-
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | Ignews</title>
+        <title>Home | Ignews</title>
       </Head>
-      <h1>
-        Hello World!
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+
+          <p>
+            Get acess to all the publications <br/>
+            <span>for $9.90 month</span> 
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
