40d4e1f0590792be16ca3bf63ec74ffdc7c85920
Gustavo Ramos <gustavoramosrock@gmail.com>
2022-01-05 23:58:08 -0300

    parameterization in routes

--- src/pages/api/users/[...params].tsx
+++ src/pages/api/users/[...params].tsx
@@ -1,7 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next"
 
 export default (request:NextApiRequest, response:NextApiResponse)=>{
-    const id = request.query.i
+    const id = request.query
+    console.log(id)
     
     const users = [
         { id: 1, name: 'Gustavo'},
