506ddcb7af48187cd25cfd5849feb97597966429
Gustavo Ramos <gustavoramosrock@gmail.com>
2022-01-13 22:40:24 -0300

    generating checkout session

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { useSession, signIn } from 'next-auth/react';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps{
@@ -5,10 +6,24 @@ interface SubscribeButtonProps{
 }
 
 export function SubscribeButton({priceId}:SubscribeButtonProps){
+    const {data: session} = useSession()
+    function handleSubscribe(){
+        if(!session){
+            signIn('github')
+            return;
+        }
+
+
+        // criar checkout session
+        
+    }
+
     return(
+    
         <button 
             type="button"
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
             Subscribe Now
         </button>
---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react'
+import { stripe } from '../../services/stripe'
+
+export default async(req: NextApiRequest, res: NextApiResponse) {
+    if(req.method === 'POST') {
+        const session = await getSession({ req })
+
+        const stripeCustomer =  await stripe.customers.create({
+            email: session.user.email,
+            // metadata:
+        })
+
+        const stripeCheckoutSession = await stripe.checkout.sessions.create({
+            customer: stripeCustomer.id,
+            
+            payment_method_types: ['card'],
+            billing_address_collection: 'required',
+            line_items:[
+                { price: 'price_1KDMOsEDYtdK8BteWqVZn2ip', quantity:1 }
+            ],
+            mode: 'subscription',
+            allow_promotion_codes: true,
+            success_url: process.env.STRIPE_SUCCESS_URL,
+            cancel_url: process.env.STRIPE_CANCEL_URL
+        })
+
+        return res.status(200).json({sessionId: stripeCheckoutSession.id})
+    }else{
+        res.setHeader('Allow', 'POST')
+        res.status(405).end('Method not allowed')
+    }
+}
