111822cfa543cb2bfbe67e2223d1cc5ffd73cf04
Gustavo Ramos <gustavoramosrock@gmail.com>
2022-01-16 03:52:28 -0300

    Avoiding Duplication in Stripe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,18 +1,58 @@
+import { query as q} from 'faunadb'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from 'next-auth/react'
+import { fauna } from '../../services/fauna'
 import { stripe } from '../../services/stripe'
+
+
+type User = {
+    ref: {
+        id: string;
+    }
+    data: {
+        stripe_costumer_id: string;
+    }
+}
 
 export default async function checkoutSession(req: NextApiRequest, res: NextApiResponse) {
     if(req.method === 'POST') {
         const session = await getSession({ req })
 
-        const stripeCustomer =  await stripe.customers.create({
-            email: session.user.email,
-            // metadata:
-        })
+        const user = await fauna.query<User>(
+            q.Get(
+                q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(session.user.email)
+                )
+            )
+            
+        )
+
+        let customerId = user.data.stripe_costumer_id
+
+        if(!customerId){
+            const stripeCustomer =  await stripe.customers.create({
+                email: session.user.email,
+                // metadata:
+                
+            })
+    
+            await fauna.query(
+                q.Update(
+                    q.Ref(q.Collection('users'), user.ref.id),{
+                        data: { 
+                            stripe_costumer_id: stripeCustomer.id,
+                        }
+                    }
+                )
+            )
+
+            customerId = stripeCustomer.id
+        }
+
 
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: stripeCustomer.id,
+            customer: customerId,
             
             payment_method_types: ['card'],
             billing_address_collection: 'required',
