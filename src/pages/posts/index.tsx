9ea9e090ace1bcd08c2bed7facfcaa9fa95b4f8d
Gustavo Ramos <gustavoramosrock@gmail.com>
2022-02-01 23:52:01 -0300

    Page: Post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -5,6 +5,7 @@ import styles from "./styles.module.scss";
 import { GetStaticProps } from "next";
 
 import { RichText } from 'prismic-dom'
+import Link from "next/link";
 
 type Post = {
 	slug: string;
@@ -27,13 +28,15 @@ export default function Posts({ posts }: PostsProps) {
                 <div className={styles.posts}>
 
 					{ posts.map(post => (
-						<a key={post.slug} href="#">
-							<time>{post.updatedAt}</time>
-							<strong>{post.title}</strong>
-							<p>
-								{post.excerpt}
-							</p>
-						</a>
+						<Link href={`/posts/${post.slug}`}>
+							<a key={post.slug}>
+								<time>{post.updatedAt}</time>
+								<strong>{post.title}</strong>
+								<p>
+									{post.excerpt}
+								</p>
+							</a>
+						</Link>
 					)) }
 
                 </div>
