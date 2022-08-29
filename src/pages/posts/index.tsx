10fa46522d9e7cf7944612b287b4ddac19f35d2b
Gustavo Ramos <gustavoramosrock@gmail.com>
2022-08-29 04:04:15 -0300

    Adicionando preview

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -28,7 +28,7 @@ export default function Posts({ posts }: PostsProps) {
                 <div className={styles.posts}>
 
 					{ posts.map(post => (
-						<Link key={`${post?.slug}`} href={`/posts/${post?.slug}`}>
+						<Link key={`${post?.slug}`} href={`/posts/preview/${post?.slug}`}>
 							<a>
 								<time>{post?.updatedAt}</time>
 								<strong>{post?.title}</strong>
